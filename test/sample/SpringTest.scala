460150aef5d736045f866b05fa669800c27e173a
ouertani <ouertani@gmail.com>
2013-07-08 00:37:22 +0200

    some updates

---
+++ test/sample/SpringTest.scala
@@ -0,0 +1,12 @@
+package sample
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ouertani
+ * Date: 07/07/13
+ * Time: 23:57
+ * To change this template use File | Settings | File Templates.
+ */
+class SpringTest {
+
+}
